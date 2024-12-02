a6a9d5b864bf869467880f2463e9b39deb9ca818
simonbws <simonbws6@gmail.com>
2024-12-02 01:57:32 +0100

    Details Section and Cart Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/Home/HomePage";
 import ProductsPage from "./components/Products/ProductsPage";
+import SingleProductPage from "./components/SingleProduct/SingleProductPage";
+import CartPage from "./components/Cart/CartPage";
 const App = () => {
   return (
     <div className="app">
@@ -10,6 +12,8 @@ const App = () => {
       <main>
         <HomePage />
         <ProductsPage />
+        <SingleProductPage />
+        <CartPage />
       </main>
     </div>
   );
---
+++ src/components/Cart/CartPage.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import "./CartPage.css";
+import remove from "../../assets/remove.png";
+import user from "../../assets/user.webp";
+import Table from "../Common/Table";
+import QuantityInput from "../SingleProduct/QuantityInput";
+
+const CartPage = () => {
+  return (
+    <section className="align_center cart_page">
+      <div className="align_center user_info">
+        <img src={user} alt="user profile" />
+        <div>
+          <p className="user_name">Szymon</p>
+          <p className="user_email">szymon@gmail.com</p>
+        </div>
+      </div>
+
+      <Table headings={["Item", "Price", "Quantity", "Total", "Remove"]}>
+        <tbody>
+          <tr>
+            <td>iPhone 16</td>
+            <td>$999</td>
+            <td className="align_center table_quantity_input">
+              <QuantityInput />
+            </td>
+            <td>$999</td>
+            <td>
+              <img
+                src={remove}
+                alt="remove icon"
+                className="cart_remove_icon"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <table className="cart_bill">
+        <tbody>
+          <tr>
+            <td>Subtotal</td>
+            <td>$999</td>
+          </tr>
+          <tr>
+            <td>Shipping Charge</td>
+            <td>$5</td>
+          </tr>
+          <tr className="cart_bill_final">
+            <td>Total</td>
+            <td>$1004</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button className="search_button checkout_button">Checkout</button>
+    </section>
+  );
+};
+
+export default CartPage;
