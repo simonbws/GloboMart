01c73feab719c930e003287e30ed0b2671cc6996
simonbws <simonbws6@gmail.com>
2024-12-02 01:22:12 +0100

    Creating Product Card

---
+++ src/components/Home/FeaturedProducts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./FeaturedProducts.css";
+
+const FeaturedProducts = () => {
+  return (
+    <section className="featured_products">
+      <h2>Featured Products</h2>
+
+      <div className="align_center featured_products_list">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedProducts;
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "./HeroSection";
 import iphone from "../../assets/iphone-14-pro.webp";
 import mac from "../../assets/mac-system-cut.jfif";
+import FeaturedProducts from "./FeaturedProducts";
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
         link="/"
         image={iphone}
       />
+      <FeaturedProducts />
       <HeroSection
         title="Build the Pro setup"
         subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini."
