a3cfec4a4fa2133b9d51afe5af8fc85e4a99e7eb
simonbws <simonbws6@gmail.com>
2024-12-02 02:14:39 +0100

    Building My Order Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import HomePage from "./components/Home/HomePage";
 import ProductsPage from "./components/Products/ProductsPage";
 import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 import CartPage from "./components/Cart/CartPage";
+import MyOrderPage from "./components/MyOrder/MyOrderPage";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
-        <HomePage />
+        {/* <HomePage />
         <ProductsPage />
         <SingleProductPage />
-        <CartPage />
+        <CartPage /> */}
+        <MyOrderPage />
       </main>
     </div>
   );
