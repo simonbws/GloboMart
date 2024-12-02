23fe36812331cc427957c30ee151368afd5a1306
simonbws <simonbws6@gmail.com>
2024-12-02 01:32:39 +0100

    Building Product Image

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/Home/HomePage";
+import ProductsPage from "./components/Products/ProductsPage";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
         <HomePage />
+        <ProductsPage />
       </main>
     </div>
   );
