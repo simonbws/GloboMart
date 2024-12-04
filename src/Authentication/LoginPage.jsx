d1c3462687972103fd8df5afe7800d534fe73aa1
simonbws <simonbws6@gmail.com>
2024-12-04 02:33:38 +0100

    Form Validation

--- src/Authentication/LoginPage.jsx
+++ src/Authentication/LoginPage.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (formData) => console.log(formData);
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit(onSubmit)}>
         <h2>Login Form</h2>
         <div className="form_inputs">
           <div>
@@ -14,7 +22,16 @@ const LoginPage = () => {
               id="name"
               className="form_text_input"
               placeholder="Enter your name"
+              {...register("name", { required: true, minLength: 3 })}
             />
+            {errors.name?.type === "required" && (
+              <em className="form_error">Please enter your name</em>
+            )}
+            {errors.name?.type === "minLength" && (
+              <em className="form_error">
+                Name should be 3 or more characters
+              </em>
+            )}
           </div>
           <div>
             <label htmlFor="phone">Phone Number</label>
@@ -23,6 +40,7 @@ const LoginPage = () => {
               id="phone"
               className="form_text_input"
               placeholder="Enter your phone number"
+              {...register("phone", { valueAsNumber: true })}
             />
           </div>
           <button type="submit" className="search_button form_submit">
