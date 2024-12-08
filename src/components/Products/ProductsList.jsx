c9704027aee2fb759c3ac49538e860e0ec6539b3
simonbws <simonbws6@gmail.com>
2024-12-08 18:31:43 +0100

    Fetching Categories

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -30,7 +30,16 @@ const ProductsList = () => {
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
         {products.map((product) => (
-          <ProductCard key={product._id} />
+          <ProductCard
+            key={product._id}
+            id={product._id}
+            image={product.images[0]}
+            price={product.price}
+            title={product.title}
+            rating={product.reviews.rate}
+            ratingCounts={product.reviews.counts}
+            stock={product.stock}
+          />
         ))}
       </div>
     </section>
--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,21 +1,34 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
+import apiClient from "../../utils/api-client";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
 
       <div className="category_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories.map((category) => (
+          <LinkWithIcon
+            id={category._id}
+            title={category.name}
+            link={`products?category=${category.name}`}
+            emoji={`http://localhost:5000/category/${category.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
